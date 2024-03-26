6fb2794ec2de1ae03278e875609a0678316e2d3f
ben burtenshaw <ben@argilla.io>
2024-03-26 11:07:26 +0000

    trained phi 2 and inference

--- train_kto.sh
+++ train_kto.sh
@@ -1,17 +1,22 @@
 # if a python env named venv does not exist, make one
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+fi
 
-# pip install .
-# pip install accelerate wandb 
-# pip install -U peft
+source venv/bin/activate
+
+pip install .
+pip install accelerate wandb 
+pip install -U peft
 
 python examples/scripts/kto.py \
     --model_name_or_path=microsoft/phi-2 \
     --per_device_train_batch_size 8 \
-    --max_steps 1000 \
-    --learning_rate 3e-5 \
+    --max_steps 500 \
+    --learning_rate 5e-5 \
     --gradient_accumulation_steps 1 \
     --logging_steps 10 \
-    --eval_steps 500 \
+    --eval_steps 100 \
     --output_dir="phi-2-kto" \
     --optim rmsprop \
     --warmup_steps 150 \
